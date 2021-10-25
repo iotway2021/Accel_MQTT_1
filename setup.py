310973f26193061b4bfbc0d336ea9770ee681e4e
iotway <iotway2021@gmail.com>
2021-10-25 17:59:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FFTDemo_1',
+    version='',
+    packages=['FFTAccelometer'],
+    url='https://github.com/iotway2021/Accel_MQTT_1',
+    license='GPLv3',
+    author='ADMINI',
+    author_email='',
+    description=''
+)
